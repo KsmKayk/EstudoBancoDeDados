a3cd4896d0c41bb1d12e08b14bf4794c3c69f885
Kayk Santana Mascarenhas <ksm.kayk@outlook.com>
2020-10-23 14:50:19 -0300

    Criado index e post do dbTestController e adicionado moment para trabalhar com datetime e dateonly

--- src/controllers/dbTestController.js
+++ src/controllers/dbTestController.js
@@ -1,13 +1,21 @@
-const db = require("./db/models")
+const db = require("../db/models")
 const { Op } = require("sequelize");
+const moment = require('moment');
+
 
 module.exports = {
   async index(req, res) {
     const resultSelect = await db.DbTest.findAll();
+    res.json(resultSelect)
   },
   async store(req, res) {
     const {text, isTrue, integerNumber,decimal,dateOnly,dateTime} = req.body;
-    //const resultInsert = await db.DbTest.create({})
+    let dateOnlyMoment = moment(dateOnly,"DD-MM-YYYY")
+    let dateTimeMoment = moment(dateTime,"DD-MM-YYYY hh:mm:ss")
+    let dateOnlyFormated = dateOnlyMoment.format("YYYY-MM-DD")
+    let dateTimeFormated = dateTimeMoment.format("YYYY-MM-DD hh:mm:ss")
+    const resultInsert = await db.DbTest.create({text, isTrue,integerNumber,decimal,dateOnly:dateOnlyFormated,dateTime:dateTimeFormated})
+    res.json(resultInsert)
   },
   async update(req, res) {},
   async delete(req, res) {},
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 const { Router } = require("express");
+const dbTestController = require("./controllers/dbTestController")
+
 
 const routes = Router();
 
+routes.get("/db", dbTestController.index)
+routes.post("/db", dbTestController.store)
+routes.put("/db", dbTestController.update)
+routes.delete("/db",dbTestController.delete)
 
 module.exports = routes;
